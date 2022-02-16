33691cccd4fc9c2d42c5d8e39651c5efb4f6298d
duyluu51 <duyluu51@gmail.com>
2022-02-16 22:05:12 +0700

    add router dom v6

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import axios from 'axios'
+import {Routes,Route} from 'react-router-dom'
 import Header from './components/Header'
-import {Routes,Route,Link} from 'react-router-dom'
+import HomePage from './pages/HomePage';
 
 function App() {
  
   return (
     <div className="App container">
         <Header/>
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          
+        </Routes>
     </div>
   );
 }
