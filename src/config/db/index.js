eb62ddc2e6b2619e56ff90deb306b53659c86b9e
duyluu51 <duyluu51@gmail.com>
2022-02-27 14:01:37 +0700

    backEnd config db

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/wed_sale_product_dev')
+        await mongoose.connect('mongodb+srv://duyluu51:Anhvaem321@cluster0.eqcte.mongodb.net/wed_sale_product_dev?retryWrites=true&w=majority')
         console.log('connect sucessfully')
     } catch (error) {
         console.log('connect failure')
